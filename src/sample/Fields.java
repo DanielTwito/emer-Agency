34c2acada57f146bfdd76e765d1bd3c15b861266
DanielTwito <Daniel.twito7@gmail.com>
2019-06-10 21:07:06 +0300

    Initial commit

---
+++ src/sample/Fields.java
@@ -0,0 +1,33 @@
+package sample;
+
+public enum Fields {
+                     //complaint Tabel
+                     complaintId,
+                     complaintOwner,
+
+                    //complaint Request Table
+                     requestId,
+
+                    //events Table
+                    eventId,
+                    time,
+                    status,
+                    headLine,
+                    firstUpdate,
+
+                    //updates Table
+                    date,
+                    updateId,
+                    description,
+                    firsVersion,
+
+                    //userss Table
+                    userName,
+                    name,
+                    password,
+                    rank,
+                    score,
+                    userStatus,
+                    email
+
+    }
---
+++ src/sample/Tables.java
@@ -0,0 +1,3 @@
+package sample;
+
+public enum Tables {users,,updates,events,complaintsRequests,complaints}
